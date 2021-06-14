19d34f5016cd1347b32b5b50b4daff78a2f248e9
Maynard Peralta <maynperalta@gmail.com>
2021-06-13 23:03:25 -0600

    created sections and footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.scss";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            &copy;2021 Maynard Peralta - created with React.js
+        </div>
+    )
+}
