7791f80a93ac70d7ff98acc7abaaa7081d89c757
Maynard Peralta <maynperalta@gmail.com>
2021-06-28 00:29:29 -0600

    added Menu component with styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import "./app.scss";
 import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
