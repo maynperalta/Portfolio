44e31146a5089564a3f764b9c98def5fe5619ec4
Maynard Peralta <maynperalta@gmail.com>
2021-07-02 19:48:43 -0600

    added list component for portfolio and map function.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "content",
+            title: "Content"
+        }
+    ];
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
+                {list.map(item => (
+                    <PortfolioList title={item.title} />
+                ))}
+
+                {/* <li className="active">Featured</li>
                 <li>Web App</li>
                 <li>Mobile App</li>
                 <li>Design</li>
-                <li>Branding</li>
+                <li>Branding</li> */}
             </ul>
             <div className="container">
                 <div className="item">
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./portfolioList.scss";
+
+export default function PortfolioList({title}) {
+    return (
+        <li className="className">
+            {title}
+        </li>
+    )
+}
+
