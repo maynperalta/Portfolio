8e05ddba1d6bbcd4c2fcfbd7c41d041f09745f5a
Maynard Peralta <maynperalta@gmail.com>
2021-07-03 23:02:15 -0600

    created outline for contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import React from "react";
-import "./contact.scss"
+import "./contact.scss";
 
-export default function Contact () {
+export default function Contact() {
+
+    const [ message, setMessage ] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true)
+    }
+
     return (
         <div className="contact" id="contact">
-            ackjsdf
+            <div className="left">
+                <img src="assets/email.png" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact me!</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email address" />
+                    <textarea placeholder="Send me a message!"></textarea>
+                    <button type="submit">Send!</button>
+                    {message && <span>Thank you for your message!</span>}
+                </form>
+            </div>
         </div>
     )
 }
